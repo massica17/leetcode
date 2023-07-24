965eae00de9e4f800ad7a21ab5781728eaa19c25
massica17 <1935984404@qq.com.com>
2023-07-24 21:45:33 +0800

    20230724

--- 116.填充每个节点的下一个右侧节点指针.java
+++ 116.填充每个节点的下一个右侧节点指针.java
@@ -32,7 +32,7 @@ class Solution {
             for(int i = 0; i < size - 1; i++){
                 slow = fast;
                 fast = dq.pollFirst();
-                slow.right = fast;
+                slow.next = fast;
                 if(fast.left != null){
                     dq.addLast(fast.left);
                 }
