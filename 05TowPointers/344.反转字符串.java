6802e23f243a5047cef341a5549289d773cf4008
massica17 <1935984404@qq.com.com>
2023-08-12 10:25:01 +0800

    20230812

---
+++ 05TowPointers/344.反转字符串.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=java
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for (int left = 0, right = n - 1; left < right; ++left, --right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+        }
+    }
+}
+
+// @lc code=end
+
